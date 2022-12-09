bc4dd8fe9f7e6104de18bfc25b23c8519b28ebe5
Your Name <you@example.com>
2022-12-09 18:16:02 +0530

    Da - 9 Medium

---
+++ LeetCode/1026.java
@@ -0,0 +1,33 @@
+class Solution {
+    // record the required maximum difference
+    int result = 0;
+
+    public int maxAncestorDiff(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        result = 0;
+        helper(root, root.val, root.val);
+        return result;
+    }
+
+    void helper(TreeNode node, int curMax, int curMin) {
+        if (node == null) {
+            return;
+        }
+        // update `result`
+        int possibleResult = Math.max(Math.abs(curMax - node.val), Math.abs(curMin - node.val));
+        result = Math.max(result, possibleResult);
+        // update the max and min
+        curMax = Math.max(curMax, node.val);
+        curMin = Math.min(curMin, node.val);
+        helper(node.left, curMax, curMin);
+        helper(node.right, curMax, curMin);
+        return;
+    }
+}
+
+// Time Complexity : BigO(N)
+// Space Complexity : BigO(N)
+
+// Date :- 09/12/2022
